4830674b497b1fc55e1527f7ba692f8fa3bcc3aa
DesaleRohit <rohitdesale2005@gmail.com>
2026-07-01 11:34:51 +0530

    [ADDED]:Rohit - springboot project setup

---
+++ age-calculator-backend/src/main/java/com/rohit/agecalculator/AgeCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.agecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgeCalculatorBackendApplication.class, args);
+	}
+
+}
